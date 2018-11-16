d2f1decebe476dc8339e189d7cfe9da9b6a72b0e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-16 15:22:15 +0700

    [Added]: unit test api Handler

---
+++ src/api/api_test.go
@@ -0,0 +1,27 @@
+package api_test
+
+import (
+	"io/ioutil"
+	. "mini-serve/api"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Test_Handler_Call_Method_GET_Should_Be_JSON(t *testing.T) {
+	expectedResponseBody := `{"method":"GET","ping":"pong"}`
+	request := httptest.NewRequest(http.MethodGet, "/say", nil)
+	writer := httptest.NewRecorder()
+	routeTest := gin.Default()
+	routeTest.GET("/say", Handler)
+	routeTest.ServeHTTP(writer, request)
+
+	response := writer.Result()
+	actualResponseBody, _ := ioutil.ReadAll(response.Body)
+
+	if expectedResponseBody != string(actualResponseBody) {
+		t.Errorf("expect\n%s\nbut it got\n%s", expectedResponseBody, actualResponseBody)
+	}
+}
