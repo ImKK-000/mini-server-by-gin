0f57f5896d86a5fa21c1806357be7c0742aa5cd5
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-17 14:45:41 +0700

    [Edited]: move parse application argument to new function

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -12,17 +12,19 @@ type ServerConfigs struct {
 	Port int
 }
 
-// ToString - convert host and port to new format
-func (serverConfigs ServerConfigs) ToString() string {
+func (serverConfigs *ServerConfigs) parseParameter() {
+	flag.StringVar(&serverConfigs.Host, "HOST", "0.0.0.0", "Set binding ip")
+	flag.IntVar(&serverConfigs.Port, "PORT", 9990, "Set binding port")
+	flag.Parse()
+}
+
+func (serverConfigs ServerConfigs) toString() string {
 	return fmt.Sprintf("%s:%d", serverConfigs.Host, serverConfigs.Port)
 }
 
 func main() {
 	var serverConfigs ServerConfigs
-	flag.StringVar(&serverConfigs.Host, "HOST", "0.0.0.0", "Set binding ip")
-	flag.IntVar(&serverConfigs.Port, "PORT", 9990, "Set binding port")
-	flag.Parse()
-
+	serverConfigs.parseParameter()
 	miniServer := route.NewRoute()
-	miniServer.Run(serverConfigs.ToString())
+	miniServer.Run(serverConfigs.toString())
 }
